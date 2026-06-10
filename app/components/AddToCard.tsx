3f468d8c4b3ac3e31687a13337ac1e6ae61d8074
kdeb-dotcomm <eugenedebrah07@gmail.com>
2026-06-10 09:21:58 +0000

    initialization

---
+++ app/components/AddToCard.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+const AddToCard = () => {
+  return (
+    <div>
+        <button className="cursor-pointer" onClick={()=>(console.log('Testing'))}>AddToCard</button>
+      
+    </div>
+  )
+}
+
+export default AddToCard
---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import ProductCard from './components/ProductCard'
+
+const Home = () => {
+  return (
+    <main>
+      <h1 className='text-2xl text-red-500 '>This is the Homepage</h1>
+      <ProductCard />
+      
+    </main>
+  )
+}
+
+export default Home
